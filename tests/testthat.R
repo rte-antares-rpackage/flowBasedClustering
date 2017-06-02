252b00e090903b1b161f58a5f56cceb2bcca79d8
TitouanRobert <titouan.robert.pro@gmail.com>
2017-06-02 10:20:28 +0200

    test

---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+#Copyright © 2016 RTE Réseau de transport d’électricité
+
+library(testthat)
+library(flowBasedClustering)
+library(antaresRead)
+
+testthat::test_check("flowBasedClustering")
---
+++ tests/testthat/testGetVertices.R
@@ -0,0 +1,24 @@
+context("getVertices")
+
+library(data.table)
+
+
+PTDF <- fread(system.file("testdata/sommets/PTDF.csv",package = "flowBasedClustering"))
+SOMMETS <- fread(system.file("testdata/sommets/sommets_which_solver.csv",package = "flowBasedClustering"))
+
+tt <- sapply(unique(SOMMETS$Id_day), function(X){
+  sapply(unique(SOMMETS$Period),  function(Y){
+    print(Y)
+    PTDFsel <- PTDF[Id_day == X & Period == Y]
+    SOMMETSSel <- SOMMETS[Id_day == X & Period == Y,c("V1","V2","V3")]
+    SOMMETSSel <- as.matrix(SOMMETSSel)
+    SOMMETSSel <- round(SOMMETSSel, 0)
+    SOMMETSSel <- SOMMETSSel[order(SOMMETSSel[,1], SOMMETSSel[,2],SOMMETSSel[,3]),]
+    res <- getVertices(as.matrix(PTDFsel[,.SD, .SDcols = c("BE","DE","FR","NL")]), PTDFsel$RAM_0)
+    res <- round(res, 0)
+    res <- res[order(res[,1], res[,2],res[,3]),]
+    sum(res- SOMMETSSel)
+  })
+})
+expect_equal(all(tt>=-1) & all(tt<=1), TRUE)
+
