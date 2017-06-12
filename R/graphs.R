2fc5f06485513dba3f187e879a29ffa952293e11
TitouanRobert <titouan.robert.pro@gmail.com>
2017-06-12 17:50:04 +0200

    plotFlowbased

--- R/graphs.R
+++ R/graphs.R
@@ -20,7 +20,7 @@
 #' @import rAmCharts
 #' @import pipeR
 #' @import ggplot2
-#' 
+#'
 #'
 generateClusteringReport <- function(dayType, outputPath = NULL,
                              data = NULL){
@@ -200,4 +200,114 @@ clusterPlot <- function(data, country1, country2, hour, dayType,
       xlab(paste(ctry[1], "(MW)"))
 
   }
+}
+
+
+
+#' Plot flow-based domain(s)
+#'
+#' @param PTDF \code{data.frame or list} PTDF file
+#' @param country1 \code{character} country 1 possible values are : BE, DE, FR, NL
+#' @param country2 \code{character} country 2 possible values are : BE, DE, FR, NL
+#' @param domainsNames \code{character} names for domain(s), use in legend
+#' @param main \code{character} title
+#'
+#' @examples
+#'
+#' \dontrun{
+#' PTDF <- fread(system.file("dev/data/faceAllYear.csv",package = "flowBasedClustering"))
+#' #Plot unique PTDF
+#' PTDF1 <- PTDF[ Date == "01/11/2015" & Period == 1]
+#' plotFlowbased(PTDF1, country1 = "DE", country2 = "BE", domainsNames = "01/11/2015")
+#'
+#' PTDF2 <- list(PTDF[ Date == "01/11/2015" & Period == 1],
+#' PTDF[ Date == "01/11/2015" & Period == 2],
+#' PTDF[ Date == "01/11/2015" & Period == 3])
+#' plotFlowbased(PTDF2, country1 = "DE", country2 = "BE", domainsNames = c("01/11/2015 - H1",
+#' "01/11/2015 - H2", "01/11/2015 - H3"), main = "Myplot")
+#'
+#' }
+#'
+#' @export
+plotFlowbased <- function(PTDF,  country1, country2, domainsNames = NULL, main = ""){
+  #Generate data for plot
+  givePlotData <- function(PTDF, country1, country2){
+    PTDF <- data.table(PTDF)
+    vertices <- getVertices(as.matrix(PTDF[, .SD, .SDcols = c("BE", "DE", "FR", "NL")]),
+                            PTDF$RAM_0)
+    vertices <- data.frame(vertices)
+    names(vertices) <- c("BE", "DE", "FR")
+    res <- .getChull(vertices, country1, country2)
+    res <- data.frame(res)
+    res <- round(res, 1)
+    names(res) <- c(country1, country2)
+    res
+  }
+
+  #Control arguments
+  multiPDTF <- all(class(PTDF ) == "list")
+  if(!is.null(domainsNames)){
+    if(!multiPDTF){
+      if(length(domainsNames) != 1){
+        stop("Only one PTDF specify for 2 or more domainsNames")
+      }
+    }else{
+      if(length(domainsNames) != length(PTDF)){
+        stop(paste0("You must have one domainsNames specify by element of PDTF list currently you have ",
+                    length(domainsNames), " domainsNames specify for ", length(PTDF), " PTDF"))
+      }
+    }
+  }
+  if(is.null(domainsNames)){
+    if(!multiPDTF){
+      domainsNames <- "D1"
+    }else{
+      domainsNames <- paste0("D", 1:length(PTDF))
+    }
+  }
+
+  #Apply function to generate data
+  if(!multiPDTF){
+    dataToGraph <- givePlotData(PTDF, country1, country2)
+    names(dataToGraph) <- paste0(domainsNames, names(dataToGraph))
+  }else{
+    dataToGraph <- sapply(1:length(PTDF), function(increm){
+      dta <- PTDF[[increm]]
+      dttp <- givePlotData(dta, country1, country2)
+      names(dttp) <- paste0(domainsNames[increm], names(dttp))
+      dttp
+    }, USE.NAMES = FALSE, simplify = FALSE)
+    rowMax <- max(unlist(lapply(dataToGraph, nrow)))
+    dataToGraph <- lapply(dataToGraph, function(dta){
+      if(nrow(dta)<rowMax){
+        Na <-  data.frame(rep(NA,rowMax - nrow(dta)),
+                          rep(NA,rowMax - nrow(dta)))
+        names(Na) <- names(dta)
+        rbind(dta,Na)
+      }else{
+        dta
+      }
+    })
+    dataToGraph <- do.call(cbind, dataToGraph)}
+
+  #Graph creation for more exmples see rAmCharts::runExamples()
+  graphs <- sapply(1:length(domainsNames), function(X){
+    amGraph(title = domainsNames[X], balloonText =
+              paste0('<b>',domainsNames[X],'<br>', paste0(domainsNames[X], country1), '</b> :[[x]] <br><b>',paste0(domainsNames[X], country2), '</b> :[[y]]'),
+            bullet = 'circle', xField = paste0(domainsNames[X], country1),yField = paste0(domainsNames[X], country2),
+            lineAlpha = 1, bullet = "bubble", bulletSize = 4,
+            lineThickness = 3)
+
+  }, USE.NAMES = FALSE)
+  pipeR::pipeline(
+    amXYChart(dataProvider = dataToGraph),
+    addTitle(text = main),
+    setGraphs(graphs),
+    setChartCursor(),
+    addValueAxes(title = paste(country1, "(MW)"), position = "bottom", minimum = -8000, maximum = 8000),
+    addValueAxes(title =  paste(country2, "(MW)"), minimum = -8000, maximum = 8000),
+    setExport(enabled = TRUE),
+    setLegend(enabled = TRUE)
+  )
+
 }
