6e5563d922c733c888a179e498f094a860b491a7
Baptiste <baptiste.seguinot@rte-france.com>
2017-08-08 15:28:21 +0200

    Update existing tests and add test on pdtfToVertices

--- tests/testthat/testClassif.R
+++ tests/testthat/testClassif.R
@@ -1,9 +1,15 @@
-context("Classif")
+context("Function getDistMatrix")
 library(data.table)
-data <- fread(system.file("testdata/VerticesTest.txt",package = "flowBasedClustering"))
 
-data <- data[,list(out = list(cbind(BE, DE, FR))), by = c("Date", "Period")]
-data[,mesh := list(flowBasedClustering:::.getMesh(out[[1]])),by = c("Date", "Period") ]
-ditMat <- flowBasedClustering:::.getDistMatrix(data)
 
-expect_equal(class(ditMat), "dist")
+test_that("Function getDistMatrix works and return appropriate object", {
+
+  data <- fread(system.file("testdata/VerticesTest.txt",package = "flowBasedClustering"))
+
+  data <- data[,list(out = list(cbind(BE, DE, FR))), by = c("Date", "Period")]
+  data[,mesh := list(flowBasedClustering:::.getMesh(out[[1]])),by = c("Date", "Period") ]
+  
+  ditMat <- flowBasedClustering:::.getDistMatrix(data, rep(1,24))
+  expect_equal(class(ditMat), "dist")
+
+})
--- tests/testthat/testGetVertices.R
+++ tests/testthat/testGetVertices.R
@@ -1,32 +1,35 @@
-context("getVertices")
-
+context("Function getVertices")
 library(data.table)
 
 
-PTDF <- fread(system.file("testdata/sommets/PTDF.csv",package = "flowBasedClustering"))
-SOMMETS <- fread(system.file("testdata/sommets/sommets_which_solver.csv",package = "flowBasedClustering"))
+test_that("Test that object returned by getVertices is convex and that it fits expected results on a small dataset", {
+  
 
-tt <- sapply(unique(SOMMETS$Id_day), function(X){
-  sapply(unique(SOMMETS$Period),  function(Y){
-    print(Y)
-    PTDFsel <- PTDF[Id_day == X & Period == Y]
-    SOMMETSSel <- SOMMETS[Id_day == X & Period == Y,c("V1","V2","V3")]
-    SOMMETSSel <- as.matrix(SOMMETSSel)
-    SOMMETSSel <- round(SOMMETSSel, 0)
-    SOMMETSSel <- SOMMETSSel[order(SOMMETSSel[,1], SOMMETSSel[,2],SOMMETSSel[,3]),]
-    res <- getVertices(as.matrix(PTDFsel[,.SD, .SDcols = c("BE","DE","FR","NL")]), PTDFsel$RAM_0)
+  PTDF <- fread(system.file("testdata/sommets/PTDF.csv",package = "flowBasedClustering"))
+  SOMMETS <- fread(system.file("testdata/sommets/sommets_which_solver.csv",package = "flowBasedClustering"))
 
-    #Test convexity
-    expect_equal(all(apply(res, 1, function(VV){
-      c(VV, -sum(VV))%*%t(as.matrix(PTDFsel[,.SD, .SDcols = c("BE","DE","FR", "NL")]))<PTDFsel$RAM_0+2
-    })), TRUE)
+  tt <- sapply(unique(SOMMETS$Id_day), function(X){
+    sapply(unique(SOMMETS$Period),  function(Y){
+      #print(Y)
+      PTDFsel <- PTDF[Id_day == X & Period == Y]
+      SOMMETSSel <- SOMMETS[Id_day == X & Period == Y,c("V1","V2","V3")]
+      SOMMETSSel <- as.matrix(SOMMETSSel)
+      SOMMETSSel <- round(SOMMETSSel, 0)
+      SOMMETSSel <- SOMMETSSel[order(SOMMETSSel[,1], SOMMETSSel[,2],SOMMETSSel[,3]),]
+      res <- flowBasedClustering::getVertices(as.matrix(PTDFsel[,.SD, .SDcols = c("BE","DE","FR","NL")]), PTDFsel$RAM_0)
 
-    res <- round(res, 0)
-    res <- res[order(res[,1], res[,2],res[,3]),]
-    sum(res- SOMMETSSel)
+      #Test convexity
+      expect_equal(all(apply(res, 1, function(VV){
+        c(VV, -sum(VV))%*%t(as.matrix(PTDFsel[,.SD, .SDcols = c("BE","DE","FR", "NL")]))<PTDFsel$RAM_0+2
+      })), TRUE)
+
+      res <- round(res, 0)
+      res <- res[order(res[,1], res[,2],res[,3]),]
+      sum(res- SOMMETSSel)
+    })
   })
+
+  expect_equal(all(tt>=-1) & all(tt<=1), TRUE)
+
 })
-
-expect_equal(all(tt>=-1) & all(tt<=1), TRUE)
-
 
