fe00aa8f3b8f11ee0e51ca2e794e9b48f7236dd5
Baptiste <baptiste.seguinot@rte-france.com>
2017-10-23 15:43:11 +0200

    add tests on getCalendar and getSequence functions

---
+++ tests/testthat/test-getCalendar.R
@@ -0,0 +1,54 @@
+context("Function getCalendar")
+
+test_that("getSequence works", {
+  
+  dates <- getSequence("2015-11-01", "2017-01-20")
+  expect_true(as.Date("2015-11-01") %in% dates)
+  expect_true(as.Date("2015-11-02") %in% dates)
+  expect_true(as.Date("2016-06-15") %in% dates)
+  expect_true(as.Date("2017-01-20") %in% dates)
+  expect_false(as.Date("2015-10-30") %in% dates)
+  expect_false(as.Date("2017-01-21") %in% dates)
+  
+})
+
+
+test_that("getCalendar works", {
+  
+  dates <- getSequence("2016-01-01", "2016-12-31")
+  interSeasonBegin <- c("2016-03-01", "2016-10-01")
+  interSeasonEnd <- c("2016-05-31", "2016-10-31")
+  calendar <- getCalendar(dates, interSeasonBegin, interSeasonEnd)
+  
+  # test that calendar type and lenght are ok
+  expect_type(calendar, "list")
+  expect_length(calendar, 6)
+  
+  # test one day for each class
+  expect_true(as.Date("2016-10-15") %in% calendar$interSeasonWe)
+  expect_true(as.Date("2016-05-26") %in% calendar$interSeasonWd)
+  expect_true(as.Date("2016-01-09") %in% calendar$winterWe)
+  expect_true(as.Date("2016-12-09") %in% calendar$winterWd)
+  expect_true(as.Date("2016-07-24") %in% calendar$summerWe)
+  expect_true(as.Date("2016-08-30") %in% calendar$summerWd)
+  
+  # test a few holidays
+  expect_false(as.Date("2016-11-11") %in% calendar$winterWd)
+  expect_false(as.Date("2016-03-28") %in% calendar$interSeasonWd)
+  #does not work (issure #28)
+  #calendar_wo_holidays <- getCalendar(dates, interSeasonBegin, interSeasonEnd, holiday = c())
+  calendar_wo_holidays <- getCalendar(dates, interSeasonBegin, interSeasonEnd, holiday = c("NewYearsDay"))
+  expect_true(as.Date("2016-11-11") %in% calendar_wo_holidays$winterWd)
+  expect_true(as.Date("2016-03-28") %in% calendar_wo_holidays$interSeasonWd)
+  
+  # test dayExclude functionality
+  calendar_wo_november <- getCalendar(dates, interSeasonBegin, interSeasonEnd, dayExclude = getSequence("2016-11-01", "2016-11-30"))
+  expect_true(as.Date("2016-11-22") %in% calendar$winterWd)
+  expect_false(as.Date("2016-11-22") %in% calendar_wo_november$winterWd)
+  
+  # test dayInWeekEnd functionnality
+  calendar_with_3days_WE <- getCalendar(dates, interSeasonBegin, interSeasonEnd,  dayInWeekend = c(1, 6, 7))
+  expect_false(as.Date("2016-07-11") %in% calendar$summerWe) # monday
+  expect_true(as.Date("2016-07-11") %in% calendar_with_3days_WE$summerWe) # monday
+  
+})
