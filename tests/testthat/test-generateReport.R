468ab2714d2caa32fe2e6ef80dc9c3af6fd269fd
Baptiste <baptiste.seguinot@rte-france.com>
2017-11-03 18:49:25 +0100

    Add temporary file to write report in tests

--- tests/testthat/test-generateReport.R
+++ tests/testthat/test-generateReport.R
@@ -2,5 +2,6 @@ context("Function generateClusteringReport")
 
 test_that("Function generateClusteringReport works", {
   clusterTD <- readRDS(system.file("dataset/cluster_example.RDS",package = "flowBasedClustering"))
-  generateClusteringReport(dayType = 2, data = clusterTD)
+  temp_directory <- tempdir()
+  generateClusteringReport(dayType = 2, data = clusterTD, outputFile = temp_directory)
 })
